8559b378030a58c85ef6b8ae7fbd563933e480f3
Yun <yun@zipcoders-MacBook-Pro-2.local>
2022-02-24 23:05:37 -0500

    I added changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,38 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Main Main_object = new Main();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int newN = scanner.nextInt();
+
+        int result = Main_object.sumOfNumbers(newN);
+        System.out.println(result);
 
     }
-}
+
+    int sumOfNumbers(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+
+
+
+
+
+
+
+    }
+
+
+
